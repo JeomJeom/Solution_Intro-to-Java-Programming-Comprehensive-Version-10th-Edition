13b4f18ffa54428268e594a909a8b45e8d5fd389
Jeom Jeom <llzeom@gmail.com>
2019-12-11 02:56:19 +1100

    This is my answer to Chapter 1 - Programming Exercises Question 1.1

---
+++ src/Exercise01_01/Exercise01_01.java
@@ -0,0 +1,22 @@
+/*
+Qn1.1 (Display three messages) Write a program that displays Welcome to Java,
+Welcome to Computer Science, and Programming is fun.
+ */
+package Exercise01_01;
+
+/**
+ *
+ * @author lina
+ */
+public class Exercise01_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+    
+}
